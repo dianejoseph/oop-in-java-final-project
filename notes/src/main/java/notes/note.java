5144dcad1d6020f256673e2762d55f6c61ec847c
rosgarci1 <150745926+rosgarci1@users.noreply.github.com>
2025-04-26 22:51:57 -0500

    ideas?

---
+++ notes/src/main/java/notes/note.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args){
+        // create new note
+
+        // static note size
+
+        // input text for note
+
+        // edit note
+        // delete note 
+
+        // customize note
+        // color
+        // font, maybe font color and highlighting
+        // opacity? 
+        // to-do list function
+        //
+
+        /* maybe a time-stamp feature. such as this note was made xx/xx/xxxx.
+         * or if they use it as a to-do list maybe do a countdown feature (ex. -3days),
+        */
+
+    }
+}
